26829913b705ec65fa7ae9ea9e69855aa16b0c85
soori-genpact <palakala.surenderreddy@genpact.com>
2026-06-24 17:01:14 +0530

    2 NEW Playbooks

---
+++ src/fluent/generated/automation/playbook/sys_pd_process_definition_c07a1f1eb94cea76ac46c79e2fc7c0e2.now.ts
@@ -0,0 +1,28 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['c07a1f1eb94cea76ac46c79e2fc7c0e2'],
+    table: 'sys_pd_process_definition',
+    data: {
+        access: 'public',
+        active: 'true',
+        allow_as_nested: 'true',
+        data_retention_period_override: '6_week',
+        description: 'Quote & Bind',
+        execution_type: 'on_demand',
+        label: 'Quote & Bind',
+        launcher_show_record_form: 'false',
+        name: 'quote_and_bind_case',
+        permission: '{"VIEW":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}],"LANE_ADD_OPTIONAL_ACTIVITY":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}],"CANCEL":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}],"RESTART":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}],"LANE_RESTART":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}],"ACTIVITY_RESTART":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}],"LAUNCH":[{"source":"USER_ROLE","id":"b0593b350a0a0aa7001d689e4542dc28"}]}',
+        process_type: '52c6d3eeff1be2102c2fffffffffff44',
+        public_access: 'false',
+        restartable: 'RESTARTABLE_TRUE',
+        run_on_pad_engine: 'false',
+        run_strategy: 'run_once',
+        run_strategy_on_process_definition: 'true',
+        schema_version: '3',
+        start_non_blocking: 'true',
+        status: 'draft',
+        view_type: 'DIAGRAM',
+    },
+})
---
+++ src/fluent/generated/other/sys-pd-lane/sys_pd_process_definition_c07a1f1eb94cea76ac46c79e2fc7c0e2.now.ts
@@ -0,0 +1,17 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['1184dda7965d60a0c39b5a52fa2ad1da'],
+    table: 'sys_pd_lane',
+    data: {
+        active: 'true',
+        description: 'Quote & Bind',
+        label: 'Quote & Bind',
+        name: 'quote_&_bind',
+        order: '1',
+        permission: '{}',
+        process_definition: 'c07a1f1eb94cea76ac46c79e2fc7c0e2',
+        restart_rule: 'RUN_ONLY_ONCE',
+        start_rule_name: 'immediate',
+    },
+})
